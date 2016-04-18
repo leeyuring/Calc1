51f5026bf8e40e02df9d8d0abc7ed8c32fb3a3f2
Seongkoo lee <koo5855@naver.com>
2016-04-18 14:10:36 +0900

    tell
    tell

--- src/main/java/asdf.java
+++ src/main/java/asdf.java
@@ -4,6 +4,10 @@ public class asdf {
 		System.out.println("hello ~" +msg);
 		System.out.println("hi ^^"+msg);
 	}
+	public void tell(String msg){
+		System.out.println("hello ~" +msg);
+		System.out.println("hi ^^"+msg);
+	}
 	public static void main(String[] args){
 		Calc h = new Calc();
 		h.say(" Gildong!");
