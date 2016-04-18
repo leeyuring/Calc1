e8887f4b25ee60c4363ff2b883ca7b4c95be5ab4
Seongkoo lee <koo5855@naver.com>
2016-04-18 13:37:35 +0900

    hello 데코레이션 추가
    hello 데코레이션 추가

--- src/main/java/asdf.java
+++ src/main/java/asdf.java
@@ -1,7 +1,7 @@
 
 public class asdf {
 	public void say(string msg){
-		System.out.println(msg);
+		System.out.println("hello ~" +msg);
 	}
 	public static void main(string[] args){
 		Hello h = new Hello();
