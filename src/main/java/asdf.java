83506fa8bd1bde7f5b325d714bd4fbeeea0598a8
Seongkoo lee <koo5855@naver.com>
2016-04-18 13:27:45 +0900

    Hello를 추가 하였음

---
+++ src/main/java/asdf.java
@@ -0,0 +1,12 @@
+
+public class asdf {
+	public void say(string msg){
+		System.out.println(msg);
+	}
+	public static void main(string[] args){
+		Hello h = new Hello();
+		h.say("hello Gildong!");
+	
+	}
+
+}
